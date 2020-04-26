896f99dd32a53427c274c25d5b36cedd35f45d59
RafaelAugusto2019 <rafaelaugusto2019@gmail.com>
2020-04-26 10:40:03 -0300

    Criando conexão com BD mysql

---
+++ config/conectionDB.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  password : '2047',
+  database : 'testeapi'
+})
+
+try {
+  connection.connect()
+} catch (error) {
+  console.log("Erro ao conectar com o banco de dados!")
+  console.log(error)
+}
+
+module.exports = (connection)
